fa2a3fb1ef9b8f40bae89bdf5c44a70c453af28e
Luiz Santos <luizaug10@gmail.com>
2022-03-05 19:45:50 -0300

    commit

--- Scripts.js
+++ Scripts.js
@@ -1,1 +1,17 @@
-function insert(a){var b=document.getElementById("resultado").innerHTML;document.getElementById("resultado").innerHTML=b+a}function clean(){document.getElementById("resultado").innerHTML=""}function back(){var a=document.getElementById("resultado").innerHTML;document.getElementById("resultado").innerHTML=a.substring(0,a.length-1)}function calcular(){var resultado=document.getElementById("resultado").innerHTML;document.getElementById("resultado").innerHTML=resultado?eval(resultado):"Nada..."}
+function insert(a) {
+  var b = document.getElementById("resultado").innerHTML;
+  document.getElementById("resultado").innerHTML = b + a;
+}
+function clean() {
+  document.getElementById("resultado").innerHTML = "";
+}
+function back() {
+  var a = document.getElementById("resultado").innerHTML;
+  document.getElementById("resultado").innerHTML = a.substring(0, a.length - 1);
+}
+function calcular() {
+  var resultado = document.getElementById("resultado").innerHTML;
+  document.getElementById("resultado").innerHTML = resultado
+    ? eval(resultado)
+    : "Nada...";
+}
